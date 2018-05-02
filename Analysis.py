c41f430f0c4d361a75d5a4a2f668200af7b98fbd
MandanS <mandansharma@yahoo.com>
2018-05-02 13:53:37 -0700

    Create Analysis.py

---
+++ Analysis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+###########For mac only############
+import matplotlib
+matplotlib.use('TkAgg')
+################################
+
+import numpy as np
+import matplotlib.pyplot as plt
